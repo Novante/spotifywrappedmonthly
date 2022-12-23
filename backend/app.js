cdd282a7b4e603fdef436bfeac3e5fbeec657745
Novante <78790665+Novante@users.noreply.github.com>
2022-12-23 18:40:08 +0100

    Spotify Bearer token is now saved in localStorage.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import querystring from 'querystring'
 import cors from 'cors'
+import reqExpress from 'request'
 
 const app = express()
 
@@ -14,8 +15,29 @@ app.get('/login', (req, res) => {
         querystring.stringify({
             response_type: 'code',
             client_id: 'ffd6a605a1f34869988b79ef09d521e9',
-            redirect_uri: 'http://localhost:3000/',
+            scope: 'user-top-read',
+            redirect_uri: 'http://localhost:3001/callback',
         }));
+})
+
+app.get('/callback', (req, res) => {
+    let access_token
+    let authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: req.query.code,
+                redirect_uri: 'http://localhost:3001/callback',
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer('ffd6a605a1f34869988b79ef09d521e9' + ':' + 'ea88943fb227464faf2a527cb8400972').toString('base64'))
+            },
+            json: true
+        }
+
+    reqExpress.post('https://accounts.spotify.com/api/token', authOptions, function(error, request, body){
+        res.redirect(`http://localhost:3000/?token=${body.access_token}`)
+    })
 })
 
 app.listen(port, () => {
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,12 +1,22 @@
-import {handleLogin} from '../utils/loginMethods'
+import {useEffect, useState} from "react";
+import {Navigate} from "react-router-dom";
 
 const Homepage = () => {
 
+    useEffect(() => {
+        const queryString = window.location.search
+        const urlParams = new URLSearchParams(queryString)
+        const token = urlParams.get('token')
+        localStorage.setItem('token', token)
+    }, [])
+
+
+
     return (
         <div>
-            <a href="http://localhost:3001/login" className="loginButton">
+            {<a href="http://localhost:3001/login" className="loginButton">
                 <p>Authorize via Spotify</p>
-            </a>
+            </a>}
         </div>
     )
 
