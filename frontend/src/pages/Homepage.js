d68e9a181f474a258610e7526b26c20816b64dc7
Novante <78790665+Novante@users.noreply.github.com>
2022-12-23 15:40:30 +0100

    Added button and some general styling

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+const Homepage = () => {
+
+return (
+    <div>
+        <button className="loginButton">
+            <p>Authorize via Spotify</p>
+        </button>
+    </div>
+)
+
+}
+
+export default Homepage
