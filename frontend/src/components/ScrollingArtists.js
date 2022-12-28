dd8fb54176ef1bf7ef1306f9e80ef8611acd5812
Novante <78790665+Novante@users.noreply.github.com>
2022-12-28 01:09:35 +0100

    Changed img onload to eventlistener, made it easy to add image to grid if grid is not full.

--- frontend/src/components/ScrollingArtists.js
+++ frontend/src/components/ScrollingArtists.js
@@ -4,27 +4,71 @@ const ScrollingArtists = (props) => {
 
     const testRef = useRef()
     const [filteredLength, setFilteredLength] = useState(props.filtered.length)
-    const [addedImageArray, setAddedImageArray] = useState([])
+    const [addedImageArray, setAddedImageArray] = useState()
     const [filteredArray, setFilteredArray] = useState(props.filtered)
 
+    const [added, setAdded] = useState([])
+
+    let tempArr = []
+
     useEffect(() => {
+
         console.log(props.filtered)
         console.log(props.pxArtistImages.length)
         if (testRef.current.offsetHeight !== 0) {
             props.setContainerHeight(testRef.current.offsetHeight)
-            document.documentElement.style.setProperty('--scrollingArtistContainerEndHeight', '-' + (testRef.current.offsetHeight + 1095) + 'px')
-            // document.documentElement.style.setProperty('--scrollingArtistContainerAnimationDelay', '5s')
+
 
         }
         console.log(props.filtered)
         setFilteredArray(props.filtered)
+
+        if (props.filtered !== null) {
+            for (let i = 0; i < props.filtered.length; i++) {
+                let img = new Image()
+                img.addEventListener('load', () => {
+                    console.log(img.height)
+                    if (img.height !== 320 || img.width !== 320){
+                    } else {
+                        tempArr.push(img)
+                    }
+                })
+                img.src = props.filtered[i].images[1].url
+            }
+            setAdded(tempArr)
+        }
+
+
+
     }, [props.filtered])
 
     useEffect(() => {
-        setTimeout(() => {
-            addMissingImages()
-        }, 2000)
-    }, [])
+        if (added !== []){
+            let div = document.getElementById('scrollingPastArtistImageContainer')
+
+            for (let i = 0; i < added.length; i++) {
+                let img = document.createElement('img')
+                img.src = added[i].src
+                div.appendChild(img);
+            }
+
+            let remainder = added.length % 3
+            if (remainder !== 0){
+                let missing = 3 - remainder
+                for (let i = 0; i < missing; i++) {
+                    let img = document.createElement('img')
+                    img.src = added[Math.floor(Math.random() * added.length)].src
+                    div.appendChild(img)
+                }
+            }
+
+            document.documentElement.style.setProperty('--scrollingArtistContainerEndHeight', '-' + (div.offsetHeight + 1095) + 'px')
+            document.documentElement.style.setProperty('--scrollingArtistContainerAnimationDelay', '5s')
+        }
+
+    }, [added])
+
+
 
 
     const checkImg320 = (e) => { // fortsätt härifrån, ta alla artister och dra bort en för varje som blir removed här. Fixa % - funktion
@@ -34,34 +78,14 @@ const ScrollingArtists = (props) => {
         }
     }
 
-    const addMissingImages = () => {
-        let remainder = filteredArray.length % 3
-        let missing = 3 - remainder
 
-        if (remainder !== 0){
-            for (let i = 0; i < missing; i++) {
-                let randomNumber = Math.floor(Math.random() * filteredArray.length)
-                setAddedImageArray(() => [...addedImageArray, filteredArray[randomNumber]])
 
-            }
-        }
-
-    }
 
 
     const ref = useRef()
     return (
         <div className="scrollingPastArtistImageContainerWrapper">
             <div ref={testRef} id="scrollingPastArtistImageContainer" className="scrollingPastArtistImageContainer">
-                {filteredArray.map((test, index) => {
-                    return <img id={index} onLoad={checkImg320} src={test.images[1].url}></img>
-                })}
-
-                {addedImageArray &&
-                    addedImageArray.map((test) => {
-                    return <img src={test.images[1].url}></img>
-                })}
-
 
 
             </div>
